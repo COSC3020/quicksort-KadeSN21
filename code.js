5e50d91ead2d6e05e4b663ee5ea9fc16fbb84887
Kade Neville <116524466+KadeSN21@users.noreply.github.com>
2024-02-13 14:56:46 -0700

    Quicksort-V1

--- code.js
+++ code.js
@@ -1,3 +1,18 @@
-function quicksort(array) {
-    return array;
+function quicksort(array){
+    if (array.length < 2){
+        return array
+    }
+
+    let pivot = array[array.length - 1];
+    let leftArr = [];
+    let rightArr = [];
+
+    for (let i = 0; i < array.length - 1; i++){
+        if (array[i] < pivot){
+            leftArr.push(array[i]);
+        } else{
+            rightArr.push(array[i]);
+        }
+    }
+    return [...quicksort(leftArr), pivot, ...quicksort(rightArr)];
 }
